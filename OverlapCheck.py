a9f31f78b781bef7609c9db2cd19154fff4d1d80
Binbin Dong <binbin.dong1994@gmail.com>
2021-07-26 05:36:00 +0200

    removed dibjet overlappting events from dijet

--- OverlapCheck.py
+++ OverlapCheck.py
@@ -2,17 +2,12 @@ import ROOT
 import sys
 import numpy as np
 from array import array
+from datetime import datetime
 
 import argparse
-
-parser = argparse.ArgumentParser(description = "input dijet and dibijet root file")
-parser.add_argument('-j', '--dijet', help='dijet input file')
-parser.add_argument('-b', '--dibjet', help='dibjet input file')
-parser.add_argument('-o', '--output', help='output file')
+parser = argparse.ArgumentParser(description='output file')
+parser.add_argument('-o', '--output', type=str)
 args = parser.parse_args()
-
-f_dijet = ROOT.TFile.Open(args.dijet, 'READ')
-f_dibjet = ROOT.TFile.Open(args.dibjet, 'READ')
 
 f_output = ROOT.TFile.Open(args.output, 'RECREATE')
 tree = ROOT.TTree('overlap','overlap')
@@ -24,31 +19,33 @@ tree.Branch('run_number', run_number, 'run_number/I')
 tree.Branch('event_number', event_number, 'event_number/I')
 tree.Branch('m_jj', m_jj, 'm_jj/F')
 
-dijetTree = f_dijet.Get('overlap')
-dibjetTree = f_dibjet.Get('overlap')
+for j in range(4):
+	f_dijet = ROOT.TFile.Open('inputData/dijetData1{}.root'.format(j+5), 'READ')
+	f_dibjet = ROOT.TFile.Open('inputData/dibjetData1{}.root'.format(j+5), 'READ')
 
-diblist = np.array([])
-for i in range(0,dibjetTree.GetEntries()):
-	dibjetTree.GetEntry(i)
-	tmplist =  [getattr(dibjetTree, 'run_number'), getattr(dibjetTree, 'event_number')]
-	diblist = np.append(diblist, tmplist)
-	if i % 5000 == 0:
-		print('processed {} events'.format(i))
+	dijetTree = f_dijet.Get('overlap')
+	dibjetTree = f_dibjet.Get('overlap')
 
-for i in range(0, dijetTree.GetEntries()):
-	dijetTree.GetEntry(i)
-	tmplist =  [getattr(dijetTree, 'run_number'), getattr(dijetTree, 'event_number')]
-	if tmplist in diblist:
-		print("kicking out an event!")
-		continue
-	run_number[0] = getattr(dijetTree, 'run_number')
-	event_number[0] =  getattr(dijetTree, 'event_number')
-	m_jj[0] = getattr(dijetTree, 'm_jj')
-	tree.Fill()
-	if i % 5000 == 0:
-		print('processed {} events'.format(i))
+	diblist = np.array([])
+	for i in range(0,dibjetTree.GetEntries()):
+		dibjetTree.GetEntry(i)
+		tmplist =  [getattr(dibjetTree, 'run_number'), getattr(dibjetTree, 'event_number')]
+		diblist = np.append(diblist, tmplist)
+		if i % 20000 == 0:
+			print('processed {} events'.format(i))
 
-
+	for i in range(0, dijetTree.GetEntries()):
+		dijetTree.GetEntry(i)
+		tmplist =  [getattr(dijetTree, 'run_number'), getattr(dijetTree, 'event_number')]
+		if tmplist in diblist:
+			print("kicking out an event!")
+			continue
+		run_number[0] = getattr(dijetTree, 'run_number')
+		event_number[0] =  getattr(dijetTree, 'event_number')
+		m_jj[0] = getattr(dijetTree, 'm_jj')
+		tree.Fill()
+		if i % 50000 == 0:
+			print('{}: processed {} events'.format(datetime.now().strftime('%H:%M:%S'),i))
 
 tree.Write("", ROOT.TObject.kOverwrite);
 f_output.Close()
