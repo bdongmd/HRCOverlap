16c5d4478e8d11cf17932a0e1b9fc0dda8fb4229
Binbin Dong <binbin.dong1994@gmail.com>
2021-08-04 02:03:09 +0200

    remove dib and other overlapping events together

--- OverlapRemoval.py
+++ OverlapRemoval.py
@@ -8,15 +8,24 @@ parser = argparse.ArgumentParser(description='output file')
 parser.add_argument('-o', '--output', type=str)
 args = parser.parse_args()
 
-isDib = True
+isDib = False
 isDib2bchannel = False
 
 f_txt = open('overlapList/DijetOverlaps.txt', 'r')
 lines = f_txt.readlines()
 
-overlapList = np.empty([len(lines), 2], dtype=np.int64)
+f_dib_txt = open('', 'r')
+diblines = f_dib_txt.readlines()
+
+overlapList = np.empty([len(lines)+len(diblines), 2], dtype=np.int64)
 i_line = 0
 for line in lines:
+	tmplist = line.rstrip().split(',')
+	tmplist = [int(tmplist[0]), int(tmplist[1])]
+	overlapList[i_line] = tmplist
+	i_line = i_line + 1
+print(i_lines)
+for line in diblines:
 	tmplist = line.rstrip().split(',')
 	tmplist = [int(tmplist[0]), int(tmplist[1])]
 	overlapList[i_line] = tmplist
@@ -32,7 +41,7 @@ m_jj = array('f', [0])
 tree.Branch('run_number', run_number, 'run_number/I')
 tree.Branch('event_number', event_number, 'event_number/L')
 tree.Branch('m_jj', m_jj, 'm_jj/F')
-tree.Branch('region', region, 'region/F')
+#tree.Branch('region', region, 'region/F')
 
 for year in range(15,19):
 	if not isDib:
@@ -44,16 +53,15 @@ for year in range(15,19):
 	for i in range(0, dijetTree.GetEntries()):
 		dijetTree.GetEntry(i)
 		tmplist =  [getattr(dijetTree, 'run_number'), getattr(dijetTree, 'event_number')]
-		if i % 50000 == 0:
+		if i % 10000 == 0:
 			print('{}: processed {} / {} events'.format(datetime.now().strftime('%H:%M:%S'),i, dijetTree.GetEntries()))
-		dijetTree.GetEntry(i)
 		if isDib2bchannel and len(getattr(dijetTree, 'region')) == 1:
 			continue
 		tmplist =  [getattr(dijetTree, 'run_number'), getattr(dijetTree, 'event_number')]
 		if tmplist in overlapList:
 			continue
-		run_number[0] = getattr(dijetTree, 'run_number')
-		event_number[0] =  getattr(dijetTree, 'event_number')
+		#run_number[0] = getattr(dijetTree, 'run_number')
+		#event_number[0] =  getattr(dijetTree, 'event_number')
 		m_jj[0] = getattr(dijetTree, 'm_jj')
 		tree.Fill()
 	f_dijet.Close()
